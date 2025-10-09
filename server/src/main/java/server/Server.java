f1460e2ad1dade387411f96e410783c2ec06f98e
Matthew James <mjames46@byu.edu>
2025-10-09 10:31:06 -0600

    Static Files test passes

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,24 +1,42 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
+import io.javalin.http.Context;
+
+import java.util.Map;
 
 public class Server {
 
-    private final Javalin javalin;
+    private final Javalin server;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        server = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+        server.delete("db", ctx -> ctx.result("{}"));
+        server.post("user", ctx -> register(ctx));
 
     }
 
     public int run(int desiredPort) {
-        javalin.start(desiredPort);
-        return javalin.port();
+        server.start(desiredPort);
+        return server.port();
     }
 
     public void stop() {
-        javalin.stop();
+        server.stop();
+    }
+
+    private void register(Context ctx) {
+        var serializer = new Gson();
+        String requestJson = ctx.body();
+        var req = serializer.fromJson(requestJson, Map.class);
+
+//        call to service and register
+        var response = Map.of(
+                "username", req.get("username"),
+                "authToken", "1234");
+        ctx.result(serializer.toJson(response));
     }
 }
