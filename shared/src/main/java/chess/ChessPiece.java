89048dec3dd675f0e551eacd1ba32a383b5e5068
Matthew James <mjames46@byu.edu>
2025-10-20 22:44:16 -0600

    Made recurse limit a static final variable for code quality test purposes

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -12,12 +12,11 @@ public class ChessPiece {
 
     private final ChessGame.TeamColor pieceColor;
     private final PieceType type;
-    private static int RECURSE_LIMIT;
+    private final static int RECURSE_LIMIT = 7;
 
-    public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+    public ChessPiece(ChessGame.TeamColor pieceColor, PieceType type) {
         this.pieceColor = pieceColor;
         this.type = type;
-        RECURSE_LIMIT = 7;
     }
 
     /**
