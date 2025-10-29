38fe93aeac5bea50f243437d7e7599dc59ed09ac
Matthew James <mjames46@byu.edu>
2025-10-29 17:58:11 -0600

    Passes createUser negative test and new getUser positive test

--- server/src/test/java/dataaccess/DataAccessTest.java
+++ server/src/test/java/dataaccess/DataAccessTest.java
@@ -2,6 +2,7 @@ package dataaccess;
 
 import chess.UserData;
 import org.junit.jupiter.api.Test;
+import service.BadRequestException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,9 +26,22 @@ class DataAccessTest {
     }
 
     @Test
+    void createUserFails() throws DataAccessException {
+        SQLDataAccess db = new SQLDataAccess();
+//        No username more than 50 characters long
+        String newUsername = "SamTheFamWhoLikesSpamJamAndButteredYamOnHisHamSammiches";
+        assertThrows(DataAccessException.class, () -> db.createUser(new UserData(newUsername,
+                "myPassword", "jj@j.com")));
+    }
+
+    @Test
     void getUser() throws DataAccessException {
         SQLDataAccess db = new SQLDataAccess();
         db.createUser(user);
         assertEquals(db.getUser(user.username()), user);
+//        Make sure that passwords and emails don't have to be unique
+        var newUser = new UserData("Bob", user.password(), user.email());
+        db.createUser(newUser);
+        assertEquals(db.getUser(newUser.username()), newUser);
     }
 }
