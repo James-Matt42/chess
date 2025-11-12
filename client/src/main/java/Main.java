e1207186667c4993930b52b20031657e5c32f431
Matthew James <mjames46@byu.edu>
2025-11-12 10:09:24 -0700

    Updated basic REPL to include all logged out and logged in function stubs

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -5,9 +5,18 @@ import server.Server;
 import java.util.Scanner;
 
 public class Main {
+
+    private static int state;
+
+    static final int LOGGED_OUT = 0;
+    static final int LOGGED_IN = 1;
+    static final int IN_GAME = 2;
+
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+
+        state = LOGGED_OUT;
 
         Server server = new Server();
         int port = server.run(8080);
@@ -15,20 +24,69 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         var result = "";
-        while (!result.equals("quit")) {
+        while (!(result.equals("quit") && state == LOGGED_OUT)) {
             printPrompt();
             String line = scanner.nextLine();
+            processInput(line);
             System.out.format("You typed: %s\n", line);
             result = line;
         }
         server.stop();
     }
 
-    public void help() {
+    private static void processInput(String input) {
+        String command = input.split(" ")[0];
+        switch (state) {
+            case LOGGED_OUT -> {
+                switch (command.toLowerCase()) {
+                    case "help" -> help();
+                    case "login" -> login();
+                    case "register" -> register();
+                }
+            }
+            case LOGGED_IN -> {
+                switch (command.toLowerCase()) {
+                    case "help" -> help();
+                    case "logout" -> logout();
+                    case "create" -> createGame();
+                    case "list" -> listGames();
+                    case "play" -> playGame();
+                    case "observe" -> observeGame();
+                }
+            }
+            case IN_GAME -> {
+                System.out.println("Not yet developed");
+            }
+            default -> help();
+        }
+    }
+
+    private static void observeGame() {
+    }
+
+    private static void playGame() {
+    }
+
+    private static void listGames() {
+    }
+
+    private static void createGame() {
+    }
+
+    private static void logout() {
+    }
+
+    private static void register() {
+    }
+
+    private static void login() {
+    }
+
+    private static void help() {
 //        Print out all the different help options
     }
 
-    static private void printPrompt() {
+    private static void printPrompt() {
         System.out.print(">> ");
     }
 }
